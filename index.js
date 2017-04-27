a8a1fcbdc8cff4dabd8766416b5bb64dcf4f0f1d
justin j. moses <justinjmoses@gmail.com>
2017-04-26 23:13:45 -0400

    Adding tonal and guessing chords

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict'
 
-module.exports = {}
+const tonal = require('tonal')
+
+module.exports = {
+    objToChord(obj = {}, key = 'C3') {
+        const intervals = Object.keys(obj).map(key => {
+            if (typeof obj[key] === 'string') return 'P5'
+            else if (typeof obj[key] === 'boolean' && obj[key]) return 'M3'
+            else if (typeof obj[key] === 'boolean' && !obj[key]) return 'm3'
+            else if (typeof obj[key] === 'number') return ['M3', 'P8']
+            else if (obj[key] instanceof Date) return 'M11'
+            else if (Array.isArray(obj[key])) return 'M9'
+            else if (typeof obj[key] === 'object') return 'm7'
+            else return 'P1'
+        }).reduce((acc, cur) => acc.concat(cur), [])
+
+        const notes = tonal.harmonize(['P1'].concat(intervals), key)
+        console.log('Possible chords: ', tonal.chord.detect(notes).join(', '))
+        console.log(notes)
+        return notes
+    }
+}
